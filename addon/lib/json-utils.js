a5dbc2160ac86b326948295be624255d172ef526
AMS777 <32177639+AMS777@users.noreply.github.com>
2018-03-26 18:34:06 +0200

    Add json parse when the request is 'application/x-www-form-urlencoded'.
    The current json parse is only valid for requests of type 'application/json', so a valid parse for requests of type 'application/x-www-form-urlencoded' is added.

--- addon/lib/json-utils.js
+++ addon/lib/json-utils.js
@@ -7,10 +7,24 @@ export function stringifyJSON(json={}){
 export function jsonFromRequest(request){
   let json = {};
   if (request.requestBody) {
+    // nested try...catch statements to avoid complexity checking the content
+    // type from the headers, as key and value may have multiple formats:
+    // - Key: 'content-type', 'Content-Type'.
+    // - Value: 'application/json', 'application/vnd.api+json'.
     try {
+      // 'Content-Type': 'application/json'
       json = JSON.parse(request.requestBody);
-    } catch(e) {
-      Ember.Logger.warn(`[FakeServer] Failed to parse json from request.requestBody "${request.requestBody}" (error: ${e})`);
+    } catch (e) {
+      try {
+        // 'Content-Type': 'application/x-www-form-urlencoded'
+        json = JSON.parse(
+          '{"'
+          + decodeURIComponent(request.requestBody.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"'))
+          + '"}'
+        );
+      } catch(e) {
+        Ember.Logger.warn(`[FakeServer] Failed to parse json from request.requestBody "${request.requestBody}" (error: ${e})`);
+      }
     }
   }
 
